c084f8852fe79d4d7cc7e1f27122671413794519
Juliette Rivière <juliette@riviere.im>
2016-01-13 17:26:32 +0100

    arrete tes conneries bassam

--- config.php
+++ config.php
@@ -1,12 +1,7 @@
 <?php
 
 $host = "localhost";
-<<<<<<< HEAD
 $user = "root";
 $password = "";
-=======
-$user = "root1";
-$password = "root";
->>>>>>> 283ae36ec5fc710f567c8f9abb7c98ae893acdc2
 $database = "";
 
